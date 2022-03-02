9b601b047db1ab4c75fe1e6838dc912320a0b678
Maki Keep <99993056+keep4@users.noreply.github.com>
2022-03-02 15:55:19 +0100

    Theme toggle
    Click on any item of the dropdown list on the right to change the color theme of the page.
    The pages automatically detect the dark mode settings of the device.
    TODO: toggle the themes dropdown by clicking on Maki's star; JQuery recommended.

---
+++ script/theme-toggle.js
@@ -0,0 +1,65 @@
+// array of themes, each with an id (also class) and a display name;
+// Maki (themes[0]) is the light mode,
+// whereas Dark (themes[1]) is the dark mode
+const themes = [
+  {
+    id: "theme-maki",
+    display: "Maki"
+  }, {
+    id: "theme-dark",
+    display: "Dark"
+  }
+];
+
+const body = document.body;
+const elementThemes = document.getElementById("themes");
+
+function switchToTheme(newTheme) {
+  for (let i = 0; i < themes.length; i++) {
+    if (themes[i].id !== newTheme.id) {
+      body.classList.remove(themes[i].id);
+    }
+  }
+  if (!body.classList.contains(newTheme.id)) {
+    body.classList.add(newTheme.id);
+  }
+}
+
+// input: object of the themes array
+function createElementTheme(theme) {
+  const elementLI = document.createElement("li");
+  const elementAnchor = document.createElement("a");
+  elementAnchor.classList.add("dropdown-item", `${theme.id}`);
+  elementAnchor.id = theme.id;
+  elementAnchor.innerHTML = theme.display;
+  elementLI.appendChild(elementAnchor);
+  elementThemes.appendChild(elementLI);
+  const elementTheme = document.getElementById(`${theme.id}`);
+  elementTheme.addEventListener("click", () => {
+    switchToTheme(theme);
+  });
+}
+
+for (let i = 0; i < themes.length; i++) {
+  createElementTheme(themes[i]);
+}
+
+// detects dark mode when loading the page
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  // dark mode
+  document.getElementById(`${themes[1].id}`).click();
+} else {
+  // light mode
+  document.getElementById(`${themes[0].id}`).click();
+}
+
+// page changes theme when the device's dark mode settings change
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+    if (event.matches) {
+      // dark mode
+      document.getElementById(`${themes[1].id}`).click();
+    } else {
+      // light mode
+      document.getElementById(`${themes[0].id}`).click();
+    }
+  });
